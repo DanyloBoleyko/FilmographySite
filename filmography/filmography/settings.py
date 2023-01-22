28d4f3e6583fa1f7fec92848696b2630673e08ee
GrandMagus <grandmagusdanilka@gmail.com>
2023-01-22 22:51:03 +0200

    Added film model

--- filmography/filmography/settings.py
+++ filmography/filmography/settings.py
@@ -126,8 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 SASS_PROCESSOR_ROOT = STATIC_ROOT
 
--- filmography/src/models.py
+++ filmography/src/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genres(models.TextChoices):
+    NONE = 'Undefined'
+    ACTION = 'Action'
+    ADVENTURE = 'Adventure'
+    COMEDY = 'Comedy'
+    DRAMA = 'Drama'
+    FANTASY = 'Fantasy'
+    HORROR = 'Horror'
+    MUSICALS = 'Musicals'
+    MYSTERY = 'Mystery'
+    ROMANCE = 'Romance'
+    SCIENCE_FICTION = 'Science fiction'
+    SPORTS = 'Sports'
+    THRILLER = 'Thriller'
+    WESTERN = 'Western'
+
+
+class Film(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(blank=True, max_length=1024)
+    plot = models.CharField(blank=True, max_length=4092)
+    rating = models.FloatField()
+    producers = models.CharField(max_length=255)
+    directors = models.CharField(max_length=255)
+    actors = models.CharField(max_length=255)
+    poster = models.ImageField(upload_to="photos/%Y-%m-%d/")
+    genre = models.CharField(
+        max_length=255,
+        choices=Genres.choices,
+        default=Genres.NONE,
+    )
+    age_rating = models.FloatField()
+    announced_at = models.DateField(blank=True)
+    published_at = models.DateField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
