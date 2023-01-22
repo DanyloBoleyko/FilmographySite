21ef62b1ca816f5ffe3b62b82628457e916a7b21
DanyloBoleyko <ipz20bd-201@stu.zsea.edu.ua>
2023-01-22 23:29:33 +0200

    Updated front

--- filmography/src/views.py
+++ filmography/src/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+routes = {
+    'First': '/first',
+    'Second': '/second',
+    'Third': '/third',
+}
+
 
 def index(request):
-    return render(request, 'index.html', {'title': 'Home'})
+    return render(request, 'index.html', {
+        'title': 'Home',
+        'links': [{'name': k, 'url': v} for k, v in routes.items()]
+    })
