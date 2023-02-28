382569fd72161a1a6e7a2bec71eee9b6f3056951
DanyloBoleyko <ipz20bd-201@stu.zsea.edu.ua>
2023-02-28 14:57:47 +0200

    Added more asserts

--- filmography/src/views/film_details.py
+++ filmography/src/views/film_details.py
@@ -5,6 +5,7 @@ from src.models.franchise import Franchise
 
 
 def film_details(request, key):
+    assert key is not None, 'Key is null'
     film = Film.objects.get(id=key)
     franchise = Franchise.objects.filter(chronology__in=[film]).first()
     return render(request, 'film_details.html', {
